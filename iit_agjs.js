f50bbfd9e09c605fab4695e4a202e424026b1df3
Kpyc <kpycnuk@gmail.com>
2021-04-06 19:38:37 +0300

    testing before cleaninig

--- iit_agjs.js
+++ iit_agjs.js
@@ -7259,7 +7259,6 @@ EndUserLibraryLoader.prototype._appendWebWrapperObject = function(n, t, i, r) {
 }
 ;
 EndUserLibraryLoader.prototype._onLoadWebLibraryError = function(n) {
-    alert("NOOOOOO");
     var t = this
       , r = t.m_library
       , f = !0
